9a5f4931bf7e05d8c3f54f4cd1d2816725d6bb32
satyamJha002 <satyam073000@gmail.com>
2025-01-14 14:05:00 +0530

    cors frontend url added

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,21 @@ dotenv.config();
 
 connectDB();
 
-app.use(cors());
+const allowedOrigins = ["https://food-delievery-system.vercel.app"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
